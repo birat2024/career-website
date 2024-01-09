522f77f42673ffe08489ffbc72865faed8a4c41c
birat2024 <156018609+birat2024@users.noreply.github.com>
2024-01-09 22:43:27 +0000

    Added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,19 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(load_jobs_from_db())
+  
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if job is not None:
+        return render_template("jobpage.html", job=job)
+    else:
+        return render_template("jobpage.html", error="Job not found"), 404
+
+
+
+  
+
 
 # to connect to the server
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -25,5 +25,32 @@ def load_jobs_from_db():
   return jobs
 
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+      result = conn.execute(
+          text("SELECT * FROM jobs WHERE id = :val"),
+          {"val": id}
+      )
+
+      row = result.fetchone()
+      if row is not None:
+          # Use result.keys() to get the column names
+          job_dict = {key: getattr(row, key) if getattr(row, key) is not None else "" for key in result.keys()}
+          return job_dict
+      else:
+          return None
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    
